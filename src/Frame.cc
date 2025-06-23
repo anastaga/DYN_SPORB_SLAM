e3b33a3ba5ccbc0644e02669a0a5729530407440
anastaga <105863687+anastaga@users.noreply.github.com>
2025-06-23 13:08:01 +0300

    Update Frame.cc

--- src/Frame.cc
+++ src/Frame.cc
@@ -1,23 +1,4 @@
-/**
-* This file is part of ORB-SLAM3
-*
-* Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
-* Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
-*
-* ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
-* License as published by the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
-* the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along with ORB-SLAM3.
-* If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "Frame.h"
-
 #include "G2oTypes.h"
 #include "MapPoint.h"
 #include "KeyFrame.h"
@@ -27,19 +8,16 @@
 #include "GeometricCamera.h"
 #include "Extractors/SPextractor.h"
 #include "Matchers/SPmatcher.h"
-#include "YoloDetection.h"
 #include <thread>
+#include "YoloDetection.h"
 #include <include/CameraModels/Pinhole.h>
 #include <include/CameraModels/KannalaBrandt8.h>
 
 namespace ORB_SLAM3
 {
 
-// 下一个生成的帧的ID,这里是初始化类的静态成员变量
 long unsigned int Frame::nNextId=0;
 
-// 是否要进行初始化操作的标志
-// 这里给这个标志置位的操作是在最初系统开始加载到内存的时候进行的，下一帧就是整个系统的第一帧，所以这个标志要置位
 bool Frame::mbInitialComputations=true;
 float Frame::cx, Frame::cy, Frame::fx, Frame::fy, Frame::invfx, Frame::invfy;
 float Frame::mnMinX, Frame::mnMinY, Frame::mnMaxX, Frame::mnMaxY;
@@ -548,16 +526,16 @@ void Frame::ExtractKeyPoints(int flag, const cv::Mat &im, const int x0, const in
     if(flag==0){
         monoLeft = (*mpExtractorLeft)(im,mvKeys,mDescriptors);
 
-        InitYoloDetector();
-        if(::gYoloDetector){
-            ::gYoloDetector->ClearArea();
-            ::gYoloDetector->GetImage(im);
-            ::gYoloDetector->Detect();
+
+        if(gYoloDetector){
+            gYoloDetector->ClearArea();
+            gYoloDetector->GetImage(im);
+            gYoloDetector->Detect();
             std::vector<cv::KeyPoint> filteredKeys;
             cv::Mat filteredDesc;
             for(size_t i=0;i<mvKeys.size();++i){
                 bool drop=false;
-                for(const auto& box : ::gYoloDetector->mvDynamicArea){
+                for(const auto& box : gYoloDetector->mvDynamicArea){
                     if(box.contains(mvKeys[i].pt)){
                         drop=true; break;
                     }
@@ -570,13 +548,6 @@ void Frame::ExtractKeyPoints(int flag, const cv::Mat &im, const int x0, const in
             mvKeys.swap(filteredKeys);
             mDescriptors = filteredDesc.clone();
         }
-
-        //画出特征点图
-        // for (const cv::KeyPoint& point : mvKeys) {
-        //     cv::circle(im,point.pt,2,cv::Scalar(0, 0, 255), 0.5);
-        // }
-        // cv::imshow("Feature Points", im);
-        // cv::waitKey(0);
     }
     else
         monoRight = (*mpExtractorRight)(im,mvKeysRight,mDescriptorsRight);
